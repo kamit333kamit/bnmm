f454e7fb25f41d87c26ad105209174d85649747a
s <s@ss-MacBook-Pro.local>
2026-02-24 05:30:54 +0530

    setup theme

---
+++ app/(authenticated)/(apps)/shop/tabs/_layout.tsx
@@ -0,0 +1,6 @@
+import { Tabs } from "expo-router"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return <Tabs/>
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+presets: [require("nativewind/preset")],
+darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
